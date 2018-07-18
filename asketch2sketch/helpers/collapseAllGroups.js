5a64900eeb1781b892fc12719447b81e5892eb35
Sergey Yakunin <sergey@yakunins.com>
2018-07-19 01:08:01 +0500

    Minor fixes

--- asketch2sketch/helpers/collapseAllGroups.js
+++ asketch2sketch/helpers/collapseAllGroups.js
@@ -1,18 +1,18 @@
 export default function collapseAllGroups(context) {
-  var doc = context.document
-  var currentArtboard = doc.findCurrentArtboardGroup()
+  const doc = context.document;
+  const currentArtboard = doc.findCurrentArtboardGroup();
 
   // deselect all layers
-  context.api().selectedDocument.selectedPage.selectedLayers.clear()
+  context.api().selectedDocument.selectedPage.selectedLayers.clear();
 
-  var action = doc.actionsController().actionForID("MSCollapseAllGroupsAction")
+  const action = doc.actionsController().actionForID('MSCollapseAllGroupsAction');
 
-  if(action.validate()) {
-    action.doPerformAction(nil)
-    if(currentArtboard !== null) {
-      currentArtboard.select_byExpandingSelection(true, false)
+  if (action.validate()) {
+    action.doPerformAction(null);
+    if (currentArtboard !== null) {
+      currentArtboard.select_byExpandingSelection(true, false);
     }
   } else {
-    log("Failed to perform MSCollapseAllGroupsAction: invalid action ID.")
+    console.log('Failed to perform MSCollapseAllGroupsAction: invalid action ID.');
   }
 }
